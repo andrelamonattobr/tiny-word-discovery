11de5731afa31f7d62a39464b0cb2f17ef872963
andrelamonattobr <andrelamonattobr@gmail.com>
2025-06-19 16:42:48 -0300

    feature: Esboçando a pagina de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
     {
         path: "main",
         component: MainPageComponent
+    },
+    {
+        path: "",
+        component: LoginPageComponent
     },
     {
         path: '**',
