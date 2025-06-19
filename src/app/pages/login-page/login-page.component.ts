0b887aef9842b9a835ed25e8b609ba9f5fcf786a
andrelamonattobr <andrelamonattobr@gmail.com>
2025-06-19 17:29:30 -0300

    feature: Estilizando o background global e a pagina de login. Adicionando funcionalidade de login

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  showLogin: boolean = true;
+
+  constructor(private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  login(username: string, password: string){
+    if (username == "admin" && password == "admin"){
+      sessionStorage.setItem("logged", "yes");
+      this.router.navigate(["main"]);
+    }
+  }
 
 }
